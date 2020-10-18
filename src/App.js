e99991896977f0284e008072996f69ea2aa56adf
Jordan Rodriguez <jrodriguezm216@gmail.com>
2020-10-18 07:41:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../public/assets/css/main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section id="banner">
+				<h2>Ecosistema DATATHON</h2>
+				<p>Elaborado por el leitmotiv <br /> Data Science</p>
+				<ul class="actions">
+					<li><a href="#Problema" class="button special big">Inicio</a></li>
+				</ul>
+			</section>
+    );
+  }
+}
+
+export default App;
